c588f722b4d3eba8a0df2efee1d125db273b691e
Guest User <Guest@epicodus-27.local>
2018-04-24 15:11:52 -0700

    add method which returns coin values for a given input amount of cents

--- CoinCombinations/Models/CoinCombs.cs
+++ CoinCombinations/Models/CoinCombs.cs
@@ -22,15 +22,42 @@ namespace CoinCombs.Models
     public void ToChange()
     {
       // int [] coins = new int[4] {25, 10, 5, 1};
-
-      if (_cent>=25)
+      if (_cent==0)
       {
-        var numQuarters = _cent/25;
-        _allCoins.Add(numQuarters.ToString() + " quarter(s)");
-      } else {
-        var remainder = _cent%25;
-        _allCoins.Add(remainder.ToString());
+        _allCoins.Add("No Change!");
       }
+      else if (_cent>0 && _cent<5)
+      {
+          var numCents = _cent/1;
+          _allCoins.Add(numCents.ToString() + " penny(s)");
+      }
+      else if (_cent>=5 && _cent<10)
+      {
+          var numNickels = _cent/5;
+          var numCents = (_cent%5)/1;
+          _allCoins.Add(numNickels.ToString() + " nickel(s)");
+          _allCoins.Add(numCents.ToString() + " penny(s)");
+      }
+      else if (_cent>=10 && _cent<25)
+      {
+          var numDimes = _cent/10;
+          var numNickels = (_cent%10)/5;
+          var numCents = ((_cent%10)%5)/1;
+          _allCoins.Add(numDimes.ToString() + " dime(s)");
+          _allCoins.Add(numNickels.ToString() + " nickel(s)");
+          _allCoins.Add(numCents.ToString() + " penny(s)");
+      }
+      else if (_cent>=25) {
+        var numQuarters = _cent/25;
+        var numDimes = (_cent%25)/10;
+        var numNickels = ((_cent%25)%10)/5;
+        var numCents = (((_cent%25)%10)%5)/1;
+        _allCoins.Add(numQuarters.ToString() + " quarter(s)");
+        _allCoins.Add(numDimes.ToString() + " dime(s)");
+        _allCoins.Add(numNickels.ToString() + " nickel(s)");
+        _allCoins.Add(numCents.ToString() + " penny(s)");
+      }
+
     }
 
     public List<string> GetAll()
